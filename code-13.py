4630053e9241fe7c2f496232b8060177a96c1096
Sashwat Jain <sashwatsjain@gmail.com>
2022-10-24 16:33:42 +0530

    Update code-13.py

---
+++ code-13.py
@@ -0,0 +1,7 @@
+# Write a Python program to find the lengths 
+# of a given list of non-empty strings.
+
+a = ["this", "is", "a", "list", "of", "strings"]
+
+for i in a:
+    print(i, " : ", len(i), sep = "")
